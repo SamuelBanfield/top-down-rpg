6d95a657546b4308e758e89b42dbfff17b4aad57
SamuelBanfield <samueljbanfield@gmail.com>
2025-01-06 14:27:25 +0000

    Fix server

--- top-down-rpg-backend/server.py
+++ top-down-rpg-backend/server.py
@@ -2,9 +2,12 @@ import websockets
 import asyncio
 
 async def receive_message(websocket):
-    for message in websocket:
+    async for message in websocket:
         print(message)
 
 async def main():
-    async with websockets.serve(receive_message, "0.0.0.0", 8001):
+    async with websockets.serve(receive_message, "0.0.0.0", 8000):
         await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(main())
